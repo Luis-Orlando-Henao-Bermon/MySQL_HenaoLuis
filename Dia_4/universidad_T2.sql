4f552934c13496cb6b3f4fbc505c27809c68353a
Luis_Henao <roronoazoro3704@gmail.com>
2024-11-08 12:49:18 -0500

    feat: :sparkles: exercises of day 5

--- Dia_4/universidad_T2.sql
+++ Dia_4/universidad_T2.sql
@@ -330,3 +330,89 @@ El resultado estará ordenado de mayor a menor por el número de asignaturas.*/
 select p.id, p.nombre, p.apellido1,p.apellido2, count(asignatura.id_profesor) as 'Numero de asignaturas' from profesor p 
 left join asignatura on p.id= asignatura.id_profesor 
 group by 1,2,3,4 order by 5 desc;
+
+
+/*Devuelve todos los datos del alumno más joven.*/
+
+select timestampdiff(year,a.fecha_nacimiento,'2024-11-08') as edad,a.* from alumno a 
+order by 1 
+limit 1 ; 
+
+/*Devuelve un listado con los profesores que no están asociados a un departamento.*/
+
+select * from profesor 
+where id_departamento is null;
+
+
+/*Devuelve un listado con los departamentos que no tienen profesores asociados.*/
+
+select * from departamento 
+where departamento.id not in (select id_departamento from profesor);
+
+/*Devuelve un listado con los profesores que tienen un departamento asociado y que no imparten ninguna asignatura.*/
+
+select profesor.* from asignatura 
+right join profesor on asignatura.id_profesor =profesor.id 
+inner join departamento on profesor.id_departamento= departamento.id
+where asignatura.id_profesor is null ;
+
+/*Devuelve un listado con las asignaturas que no tienen un profesor asignado.*/
+
+select * from asignatura 
+where id_profesor is not  null;
+
+
+-- imposible
+
+
+/*Devuelve un listado con el nombre de todos los departamentos que tienen profesores que imparten alguna asignatura
+ en el Grado en Ingeniería Informática (Plan 2015).*/
+ 
+select distinct d.nombre from departamento d 
+inner join profesor p on d.id =p.id_departamento
+inner join asignatura a on p.id =a.id_profesor
+inner join grado on a.id_grado = grado.id
+where grado.nombre='Grado en Ingeniería Informática (Plan 2015)';
+ 
+ /*Devuelve un listado con los nombres de todos los profesores y los departamentos que tienen vinculados. 
+ El listado también debe mostrar aquellos profesores que no tienen ningún departamento asociado. 
+ El listado debe devolver cuatro columnas, nombre del departamento, primer apellido, segundo apellido y nombre del profesor. 
+ El resultado estará ordenado alfabéticamente de menor a mayor por el nombre del departamento, apellidos y el nombre.*/
+ 
+select d.nombre as 'Nombre de departamento',p.apellido1, p.apellido2, p.nombre from departamento d right join profesor p on d.id = p.id_departamento order by 1,2,3,4 asc;
+ 
+/*Devuelve un listado con los profesores que no están asociados a un departamento.*/
+
+select * from profesor where id_departamento is null;
+
+/*Devuelve un listado con los departamentos que no tienen profesores asociados.*/
+
+select * from departamento 
+where departamento.id not in (select id_departamento from profesor);
+
+/*Devuelve un listado con los profesores que no imparten ninguna asignatura.*/
+
+select profesor.* from asignatura 
+right join profesor on asignatura.id_profesor =profesor.id
+where asignatura.id_profesor is null ;
+
+/*Devuelve un listado con las asignaturas que no tienen un profesor asignado.*/
+
+select * from asignatura where id_profesor is null;
+
+/*Devuelve un listado con todos los departamentos que tienen alguna asignatura que no se haya impartido en ningún curso escolar. 
+El resultado debe mostrar el nombre del departamento y el nombre de la asignatura que no se haya impartido nunca.*/
+
+select d.nombre, a.nombre from departamento d 
+inner join profesor p on d.id =p.id_departamento
+inner join asignatura a on p.id =a.id_profesor 
+where a.id not in (select id_asignatura from alumno_se_matricula_asignatura);
+
+/*Devuelve un listado con todos los departamentos que no han impartido asignaturas en ningún curso escolar.*/
+
+select distinct d.nombre from departamento d 
+inner join profesor p on d.id =p.id_departamento
+inner join asignatura a on p.id =a.id_profesor 
+left join alumno_se_matricula_asignatura asm on a.id = asm.id_asignatura
+where a.id not in (select id_asignatura from alumno_se_matricula_asignatura);
+ 
